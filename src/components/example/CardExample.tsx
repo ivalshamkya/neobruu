7ee1884722a396d456de6aeff0b25f83ee70d154
ivalshamkya <ivalshamkya@gmail.com>
2024-01-23 22:30:50 +0800

    feat(card): new card component

---
+++ src/components/example/CardExample.tsx
@@ -0,0 +1,19 @@
+import { Card } from "@/components/neobruu/Card";
+import Button from "../neobruu/Button";
+
+export default function CardExample() {
+  return (
+    <div className="flex gap-3">
+      <Card rounded="lg">
+        <Card.Header imageUrl="https://picsum.photos/200" />
+        <Card.Body>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem cum nisi molestiae voluptatibus nemo eaque doloremque tempora incidunt laborum! Obcaecati ipsum ducimus ad aliquid cum delectus adipisci ratione dolores.
+        </Card.Body>
+        <Card.Footer>
+          <Button>Read More</Button>
+        </Card.Footer>
+      </Card>
+
+    </div>
+  )
+}
